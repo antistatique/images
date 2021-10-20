b00fbe07a131b0f656b6b39cb1327ebd13c1b249
Yann Gouffon <github@yago-art.ch>
2021-10-20 14:38:19 +0200

    ✨ add upload

--- src/components/Comparator.tsx
+++ src/components/Comparator.tsx
@@ -13,22 +13,45 @@ import tw from 'twin.macro';
 import options from './options.json';
 
 const Selector = ({
+  current,
   onSelect,
+  onUpload,
   side,
 }: {
+  current: number;
   onSelect: (id: number) => void;
+  onUpload: (files: FileList | null) => void;
   side: string;
 }): JSX.Element => (
-  <select
-    tw="w-1/4 pr-8 truncate rounded text-minuit"
-    onChange={e => onSelect(+e.target.value)}
-  >
-    {options.map((option, key) => (
-      <option key={`option-${side}-${key}`} value={key}>
-        {option.filename}
-      </option>
-    ))}
-  </select>
+  <>
+    <label
+      css={side === 'right' && tw`order-last`}
+      tw="flex items-center h-10 px-3 bg-white rounded cursor-pointer hover:bg-limonade transition-colors"
+      title="Use your own image"
+    >
+      ➕
+      <input
+        type="file"
+        tw="hidden"
+        onChange={e => onUpload(e.target.files)}
+        accept="image/*"
+        multiple={false}
+      />
+    </label>
+    <select
+      tw="w-1/4 pr-8 truncate rounded text-minuit"
+      css={side === 'right' ? tw`mr-2` : tw`ml-2`}
+      onChange={e => onSelect(+e.target.value)}
+      value={current}
+    >
+      <option value="-1">Choose an existing file</option>
+      {options.map((option, key) => (
+        <option key={`option-${side}-${key}`} value={key}>
+          {option.filename}
+        </option>
+      ))}
+    </select>
+  </>
 );
 
 const Comparator = (): JSX.Element => {
@@ -36,6 +59,42 @@ const Comparator = (): JSX.Element => {
   const [right, setRight] = useState(options[1]);
   const [width, setWidth] = useState(1200);
   const [height, setHeight] = useState(800);
+
+  const handleUpload = async (
+    files: null | FileList,
+    set: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  ): Promise<void> => {
+    const file = files?.[0];
+    if (file === null || file === undefined) return;
+
+    const path = URL.createObjectURL(file);
+
+    const format = file.name.split('.')[1];
+    const filename = file.name;
+    const weight = `${Math.round(file.size / 1000)}Ko`;
+    const dimensions: { width: number; height: number } = await new Promise(
+      resolve => {
+        const img = new Image();
+        img.onload = () => {
+          resolve({
+            height: img.height,
+            width: img.width,
+          });
+        };
+        img.src = path;
+      }
+    );
+
+    set({
+      format,
+      weight,
+      quality: '?',
+      width: dimensions.width,
+      height: dimensions.height,
+      filename,
+      path,
+    });
+  };
 
   return (
     <div tw="pt-12 text-white bg-minuit">
@@ -71,7 +130,12 @@ const Comparator = (): JSX.Element => {
         </p>
         <div tw="py-4 mt-6 text-lg grid grid-cols-2">
           <div tw="flex flex-wrap items-center">
-            <Selector side="left" onSelect={id => setLeft(options[id])} />
+            <Selector
+              current={options.findIndex(i => i.path === left.path)}
+              side="left"
+              onSelect={id => id > -1 && setLeft(options[id])}
+              onUpload={files => handleUpload(files, setLeft)}
+            />
             <div tw="px-4">
               <span tw="uppercase text-flamant-rose">{left.format}</span> |{' '}
               <span tw="text-limonade">
@@ -81,7 +145,12 @@ const Comparator = (): JSX.Element => {
             </div>
           </div>
           <div tw="flex flex-wrap items-center justify-end">
-            <Selector side="right" onSelect={id => setRight(options[id])} />
+            <Selector
+              current={options.findIndex(i => i.path === right.path)}
+              side="right"
+              onSelect={id => id > -1 && setRight(options[id])}
+              onUpload={files => handleUpload(files, setRight)}
+            />
             <div tw="px-4 text-right md:order-first">
               <span tw="text-magenta">{right.weight}</span> ← {right.quality}% |{' '}
               <span tw="text-limonade">
@@ -103,17 +172,9 @@ const Comparator = (): JSX.Element => {
             window.innerWidth > width && tw`left-1/2 -translate-x-1/2`,
           ]}
           changePositionOnHover
-          itemOne={
-            <ReactCompareSliderImage
-              src={`/images/${left.filename}`}
-              alt="Left image"
-            />
-          }
+          itemOne={<ReactCompareSliderImage src={left.path} alt="Left image" />}
           itemTwo={
-            <ReactCompareSliderImage
-              src={`/images/${right.filename}`}
-              alt="Right image"
-            />
+            <ReactCompareSliderImage src={right.path} alt="Right image" />
           }
         />
       </div>
