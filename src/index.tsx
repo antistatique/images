23b2c34c2c7b0237848d6d662bf198a75a072b76
Yann Gouffon <github@yago-art.ch>
2021-10-20 10:28:18 +0200

    🎉 complete bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { GlobalStyles } from 'twin.macro';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
